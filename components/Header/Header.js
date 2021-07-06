ef22164a9f1c4278db08e3921c254e6479b9fdf3
Matviienko Artem <k2141092mav@gmail.com>
2021-07-06 13:14:36 +0300

    Create folder: Components, Header

---
+++ components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Header = () => {
+    <div>
+        <ul>
+            <li>Logotip</li>
+            <li>Wallet</li>
+            <li>Name</li>
+            <li>Out</li>
+        </ul>
+    </div>
+}
+
+export default Header;
--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
 
 const DashboardPage = () => {
   return (
     <>
+      {/* <Header/> */}
       <h1>This is DashboardPage</h1>
     </>
   );
