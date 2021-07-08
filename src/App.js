8454f79a67f181f64818a66baf18a6a46623d258
Roman-Y-K <r.prohor7@gmail.com>
2021-07-08 23:00:09 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Suspense, lazy } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import s from './styles/base.module.css';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import authOps from './redux/auth/auth-operations';
+import ProtectedRoute from './components/ProtectedRoute';
 // import Spinner from './components/Spinner';
 
 const RegisterPage = lazy(() =>
@@ -10,6 +14,12 @@ const LoginPage = lazy(() => import('./pages/views/login/LoginPage'));
 const DashboardPage = lazy(() => import('./pages/DashboardPage'));
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(authOps.getCurrentUser());
+  }, [dispatch]);
+
   return (
     <>
       <div className={s.container}>
@@ -17,7 +27,7 @@ const App = () => {
           <Switch>
             <Route path="/register" component={RegisterPage} />
             <Route path="/login" component={LoginPage} />
-            <Route path="/dashboard" component={DashboardPage} />
+            <ProtectedRoute path="/dashboard">{DashboardPage}</ProtectedRoute>
           </Switch>
         </Suspense>
       </div>
