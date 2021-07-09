2a3a073aceb3137f31ee0b72f9e8c1b320949add
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2021-07-09 19:59:09 +0300

    +

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,30 +1,30 @@
-// import React from 'react';
-// import styles from './Chart.module.css';
-// import { Doughnut } from 'react-chartjs-2';
+import React from 'react';
+import styles from './Chart.module.css';
+import { Doughnut } from 'react-chartjs-2';
 
-// //--- Добавить баланс в центр пончика !
+//--- Добавить баланс в центр пончика !
 
-// //Опции для Диаграммы больше можно узнать --> https://www.chartjs.org/docs/latest/
+//Опции для Диаграммы больше можно узнать --> https://www.chartjs.org/docs/latest/
 
-// const optionsForChart = {
-//   plugins: {
-//     title: {
-//       display: false,
-//       text: 'Просто диаграмма',
-//     },
-//     legend: {
-//       display: false,
-//       position: 'bottom',
-//     },
-//   },
-// };
+const optionsForChart = {
+  plugins: {
+    title: {
+      display: false,
+      text: 'Просто диаграмма',
+    },
+    legend: {
+      display: false,
+      position: 'bottom',
+    },
+  },
+};
 
-// const Chart = ({ chartData }) => {
-//   return (
-//     <div className={styles.chart}>
-//       <Doughnut data={chartData} options={optionsForChart} />
-//     </div>
-//   );
-// };
+const Chart = ({ chartData }) => {
+  return (
+    <div className={styles.chart}>
+      <Doughnut data={chartData} options={optionsForChart} />
+    </div>
+  );
+};
 
-// export default Chart;
+export default Chart;
