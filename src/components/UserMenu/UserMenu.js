6cbdb1b50c905a1d890f1273bdac6a9ff319bf53
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2021-07-16 23:38:31 +0300

    frankenstein

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,28 +1,72 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-
+//import React from 'react';
+import { useSelector, useDispatch, connect } from 'react-redux';
 import { getUserName } from '../../redux/auth/auth-selectors';
-import { isModalLogoutOpen } from '../../redux/global/global-action';
+import authOperations from '../../redux/auth/auth-operations';
+// ../../redux/auth
+// import { isModalLogoutOpen } from '../../redux/global/global-action';
 import styles from './UserMenu.module.css';
-
-const UserMenu = () => {
+//===================================
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+//import s from './UserMenu.module.css';
+Modal.setAppElement('#root');
+const UserMenu = ({ onLogout }) => {
   const dispatch = useDispatch();
+  const [modaIsOpen, setModalisOpen] = useState(false);
   const name = useSelector(getUserName);
-  const ModalLogoutOpen = () => dispatch(isModalLogoutOpen());
-
+  // const operation = dispatch(authOperations.logOut());
+  // const [modalIsOpen, setIsOpen] = React.useState(false);
+  // const ModalLogoutOpen = () => dispatch(isModalLogoutOpen());
+  function closeModal() {
+    setModalisOpen(false);
+  }
   return (
     <div className={styles.user_menu_container}>
       <span className={styles.user_name}>{name}</span>
       <span className={styles.user_name_2}>|</span>
-
       <button
         className={styles.out_icon}
         type="button"
-        onClick={ModalLogoutOpen}
+        onClick={() => setModalisOpen(true)}
       ></button>
+      <Modal
+        isOpen={modaIsOpen}
+        onRequestClose={() => setModalisOpen(false)}
+        className={styles.modalContent}
+        overlayClassName={styles.modalOverlay}
+      >
+        <button
+          // isOpen={modaIsOpen}
+          onClick={closeModal}
+          className={styles.buttonClose}
+        >
+          Закрытие модалки
+        </button>
+        <button
+          // isOpen={modaIsOpen}
+          // onClick={closeModal}
+          type="button"
+          onClick={onLogout}
+          className={styles.buttonClose}
+        >
+          Полностью выход
+        </button>
+
+        {/* <AddTransactionForm /> */}
+      </Modal>
+      {/* <button
+        className={styles.out_icon}
+        type="button"
+        onClick={ModalLogoutOpen}
+      ></button> */}
       <span className={styles.user_out}>Выйти</span>
     </div>
   );
 };
 
-export default UserMenu;
+const mapDispatchToProps = {
+  onLogout: authOperations.logOut,
+};
+
+export default connect(null, mapDispatchToProps)(UserMenu);
+// export default UserMenu;
