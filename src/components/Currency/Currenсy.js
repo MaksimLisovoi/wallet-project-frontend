9cf5636148c72a2c97ee328b8a6ab6b6ef064f47
MaksimLisovoi <lisovoi123@gmail.com>
2023-03-28 18:24:44 +0300

    added currency request from monobank

--- src/components/Currency/Currenсy.js
+++ src/components/Currency/Currenсy.js
@@ -2,34 +2,55 @@ import { useEffect, useState } from 'react';
 import { Ellipsis } from 'react-spinners-css';
 import style from './Currency.module.css';
 import axios from 'axios';
+const cc = require('currency-codes');
 
 export default function Currency() {
   const [currency, setCurrency] = useState([]);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  async function getCurrency() {
-    try {
-      const data = await axios.get(
-        'https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5',
-      );
-      const result = await data.json();
-      console.log(result);
-      setIsLoading(true);
-      setCurrency(result);
-    } catch (e) {
-      setIsLoading(true);
-      setError(error);
-    }
-  }
+  // https://api.privatbank.ua/#p24/exchange
+
+  // async function getCurrency() {
+  //   try {
+  //     const { data } = await axios.get('https://api.monobank.ua/bank/currency');
+
+  //     // const courses = data.slice(0, 2);
+
+  //     setIsLoading(true);
+  //     setCurrency(data);
+  //   } catch (e) {
+  //     setIsLoading(true);
+  //     setError(error);
+  //   }
+  // }
 
   useEffect(() => {
-    getCurrency();
-  }, []);
+    async function getCurrency() {
+      try {
+        const { data } = await axios.get(
+          'https://api.monobank.ua/bank/currency',
+        );
 
-  const currencyFilter = currency.filter(el => {
-    return el.ccy !== 'RUR';
-  });
+        // const courses = data.slice(0, 2);
+
+        setIsLoading(true);
+        setCurrency(data);
+      } catch (e) {
+        setIsLoading(true);
+        setError(error);
+      }
+    }
+    getCurrency();
+  }, [error]);
+
+  const courses = currency.slice(0, 2);
+
+  // const currencyFilter = currency.filter(el => {
+  //   return el.currencyCodeA === 840 && el.currencyCodeA === 980;
+  // });
+
+  // console.log(currencyFilter);
 
   return (
     <div className={style.container}>
@@ -53,12 +74,25 @@ export default function Currency() {
             </p>
           </div>
         )}
-        {currencyFilter.map(({ ccy, buy, sale }) => {
+        {/* {currencyFilter.map(({ ccy, buy, sale }) => {
           const buyParse = parseFloat(buy).toFixed(2);
           const saleParse = parseFloat(sale).toFixed(2);
           return (
             <ul key={ccy} className={style.table}>
               <li>{ccy}</li>
+              <li>{buyParse}</li>
+              <li>{saleParse}</li>
+            </ul>
+          );
+        })} */}
+        {courses.map(({ currencyCodeA, rateBuy, rateSell }) => {
+          const buyParse = parseFloat(rateBuy).toFixed(2);
+          const saleParse = parseFloat(rateSell).toFixed(2);
+          const { code } = cc.number(currencyCodeA);
+
+          return (
+            <ul key={currencyCodeA} className={style.table}>
+              <li>{code}</li>
               <li>{buyParse}</li>
               <li>{saleParse}</li>
             </ul>
